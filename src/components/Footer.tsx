2970f2ea3650f2b5c3b93c72b7588828a3f5524e
Anahita Joshi <apjoshi@bu.edu>
2025-03-07 18:37:57 -0500

    added dummy link for credits in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { Link } from "react-router";
 
 const StyledFooter = styled.footer`
   display: flex;
@@ -15,11 +16,23 @@ const StyledP = styled.p`
   margin: 0;
 `;
 
+const StyledLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  font-size: calc(16px + 1vw);
+  text-align: center;
+
+  @media screen and (max-width: 750px) {
+    text-decoration: none;
+    font-size: calc(2px + 1vw);
+  }
+`;
+
 export default function Footer() {
   return (
     <StyledFooter>
       <StyledP>
-        All Rights Reserved by Anahita Joshi &copy;
+        All Rights Reserved by Anahita Joshi <StyledLink to={`/`}>Credits</StyledLink> &copy;
       </StyledP>
     </StyledFooter>
   );
